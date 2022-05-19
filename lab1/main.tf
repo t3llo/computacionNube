4af5abd88037aa42d4372c8013f9fe8394ce2577
Santiago Tello <santiago@comono.co>
2022-05-19 14:49:37 -0500

    Lab 1 completo

--- lab1/main.tf
+++ lab1/main.tf
@@ -21,7 +21,7 @@ data "aws_ami" "amazon-linux-2" {
 resource "aws_instance" "webServer" {
   ami                    = data.aws_ami.amazon-linux-2.id
   instance_type          = "t2.micro"
-  #user_data              = file("aws-user-data.sh")
+  user_data              = file("aws-user-data.sh")
   vpc_security_group_ids = [aws_security_group.webSG.id]
   subnet_id = aws_subnet.public.id
   associate_public_ip_address =  true
--- lab1/networks.tf
+++ lab1/networks.tf
@@ -1,11 +1,56 @@
 resource "aws_vpc" "MyLabVPC" {
   cidr_block = var.vpc_cidr_block
-
 }
 
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.MyLabVPC.id
   cidr_block = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.MyLabVPC.id
+
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+resource "aws_nat_gateway" "nat_gateway" {
+  subnet_id = aws_subnet.public.id
+  allocation_id = aws_eip.nat_eip.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.MyLabVPC.id
+}
+resource "aws_route" "public_internet_gateway" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.internet_gateway.id
+}
+
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_route_table.public.id
+  subnet_id = aws_subnet.public.id
+}
+
+resource "aws_security_group" "vpc_security_group" {
+  name = "VPC security group"
+  description = "SG to allow traffic from the VPC"
+  vpc_id = aws_vpc.MyLabVPC.id
+  ingress {
+    from_port = "0"
+    to_port =  "0"
+    protocol = "-1"
+    self = true
+  }
+  egress {
+    from_port = "0"
+    to_port = "0"
+    protocol = "-1"
+    self = true
+  }
 }
 
 resource "aws_security_group" "webSG" {
