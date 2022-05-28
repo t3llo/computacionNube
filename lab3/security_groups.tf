539adabc4b275d6efcb7b9cc15b7ef33ac972a0f
stellor <santiago@comono.co>
2022-05-28 16:24:11 -0500

    Load Balancer, Web Server Security group

--- lab3/security_groups.tf
+++ lab3/security_groups.tf
@@ -28,6 +28,59 @@ resource "aws_security_group" "mariadb_security_group" {
       description = "Allow DB connection"
       from_port = 3306
       to_port = 3306
-      cidr_blocks = [ "172.16.2.0/24" ]
+      security_groups = [ aws_security_group.webserver_security_group.id]
   }
+  tags = {
+    Name = "MariaDB Security group"
+  }
+
+}
+
+resource "aws_security_group" "webserver_security_group" {
+  vpc_id = aws_vpc.vpc.id
+  description = "Security group for webServers"
+
+  ingress {
+    description = "Allow SSH connections"
+    from_port = 22
+    to_port = 22
+    security_groups = [ aws_security_group.bastion_security_group.id]
+  } 
+
+  ingress {
+    description = "Allow HTTP Requests"
+    from_port = 80
+    to_port = 80
+    security_groups = [ aws_security_group.load_balancer_security_group.id ]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "WebServer Security Group"
+  }
+}
+
+resource "aws_security_group" "load_balancer_security_group" {
+  vpc_id = aws_vpc.vpc.id
+  description = "Security group for Load Balancer"
+
+  ingress {
+    description = "Allow HTTP Requests"
+    from_port = 80
+    to_port = 80
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 }
